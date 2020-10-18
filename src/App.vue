4fc48a29ca5a263d472ff1931a3ee2ba443b42b3
lei <895641390@qq.com>
2020-10-18 21:59:14 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>hi</div>
+  <Demo/>
+</template>
+
+<script>
+import Demo from './components/Demo.vue'
+export default {
+  name: 'App',
+  components: {
+    Demo
+  }
+}
+</script>
---
+++ src/components/Demo.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>this is demo</div>
+</template>
+
+<style scoped>
+div{
+    width: 100%;
+    height: 20px;
+    border: 1px solid salmon;
+}
+</style>
